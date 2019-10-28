a9aeff8309c5fea7fd19fefc2974e7a2a1ae5f75
m4rc1o <marcioinaciosantana@gmail.com>
2019-10-28 15:16:30 -0300

    Implementa a classe contador

--- utils/utils.py
+++ utils/utils.py
@@ -1,19 +1,17 @@
 
 def ler_arquivo_trace(arq, m):
     # Um dicionario de desvios onde as chaves são os endereços 
-    # dos desvios e os valores são uma lista das 
-    # direções(simula o registrador de histórico local) tomadas
-    # por esse desvio nas n execuções mais recentes
+    # dos desvios e os valores são uma lista das direções tomadas
+    # por esse desvio em toda a execução do programa
     desvios = {}
 
-    # Lê o arquivo traces e preenche desvios
+    # Lê o arquivo traces e preenche o dicionário desvios
     with open(arq) as file_trace1:
         linha = file_trace1.readline()
         while linha:       
             temp = linha.split()
             endereco_desvio_hex = temp[0]
             direcao_desvio = temp[1]
-            
             # Converte o endereço para binário e mantém apenas os m lsb com exeção dos dois primeiros bits
             endereco_desvio_bin = bin(int(endereco_desvio_hex, 16))
             endereco_desvio_bin = endereco_desvio_bin[2:33].zfill(32)
@@ -27,5 +25,16 @@ def ler_arquivo_trace(arq, m):
             linha = file_trace1.readline()
     
     return desvios
+
+class Contador:
+    def __init__ (self):
+        self.valor = 2
     
+    def incrementar(self):
+        if self.valor < 3:
+            self.valor += 1
+    
+    def decrementar(self):
+        if self.valor > 0:
+            self.valor -= 1
 
